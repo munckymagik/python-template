afbf532af93989ea8ef38675ce4be6571a7e029b
Dan Munckton <munckfish@gmail.com>
2020-04-12 11:11:15 +0100

    Support doctests

--- projname/__init__.py
+++ projname/__init__.py
@@ -1,2 +1,11 @@
 def itworks():
+    """A simple test to prove our library and tests are working together.
+
+    Here is a doctest that should run when we execute our test runner:
+
+    >>> import projname
+    >>> projname.itworks()
+    'hello'
+
+    """
     return "hello"
